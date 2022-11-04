84c61a72fd30a145759f0bcb1f65395178935a70
SweetUp <BangBang@DangDang.com>
2022-11-04 16:04:52 +0800

    Third Commit

--- src/main/java/test/git/git_test.java
+++ src/main/java/test/git/git_test.java
@@ -4,6 +4,7 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello，2");
+        System.out.println("hello，3");
 
     }
 }
