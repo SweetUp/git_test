312fa579f7b8797b63f5927c485a82a1bf8e9203
SweetUp <BangBang@DangDang.com>
2022-11-21 11:31:28 +0800

    5 Commit

--- src/main/java/test/git/git_test.java
+++ src/main/java/test/git/git_test.java
@@ -6,5 +6,6 @@ public class git_test {
         System.out.println("hello，2");
         System.out.println("hello，3");
         System.out.println("hello，4");
+        System.out.println("hello，5master");
     }
 }
